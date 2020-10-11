782a0eb3165d998dd1f1bc5f4fd48932211ad8c7
48ktk <ksao.257.urryh@gmail.com>
2020-10-11 13:56:13 +0900

    とりあえず的中入力とそれに応じた計算機能を追加

--- src/index.js
+++ src/index.js
@@ -13,23 +13,24 @@ ReactDOM.render(
  */
 function CreateEachScoreField(props) {
   // Todo 合計計算処理 該当playerの的中が変わるたび
-  let score = 0;
+  //let score = 0;
   // 描画後でないとtableを取得できない
-  useEffect(() => {
-    const table = document.getElementById("main-table");
-    const cells = table.querySelectorAll("td.player" + props.order);
-    
-    cells.forEach((cell) => {
-      if (cell.innerText == "〇") {
-        score++;
-      }
-      document.querySelector("th.player" + props.order).innerText = score;
-    });
-  });
-  return <th className={"player" + props.order}></th>;
+  // useEffect(() => {
+  //   const table = document.getElementById("main-table");
+  //   const cells = table.querySelectorAll("td.player" + props.order);
+  //   console.log(cells);
+  //   cells.forEach((cell) => {
+  //     if (cell.innerText == "〇") {
+  //       score++;
+  //     }
+  //     document.querySelector("th.player" + props.order).innerText = score;
+  //   });
+  // },[score]);
+  return <th className={"player" + props.order}>{props.score}</th>;
 }
 CreateEachScoreField.propTypes = {
   order: PropTypes.string,
+  score: PropTypes.number,
 };
 // 前立、後立の合計表示欄
 function CreateTachiTortal(props) {
@@ -46,57 +47,63 @@ CreateTachiTortal.propTypes = {
   tachi: PropTypes.string,
 };
 // 総計表示欄
-function CreateGrandTotal() {
-  // Todo 合計計算処理 playerの的中変わるたび
-  return <th>160</th>;
-}
+// function CreateGrandTotal() {
+//   // Todo 合計計算処理 playerの的中変わるたび
+//   return <th>160</th>;
+// }
 // 各種的中合計の表示欄
-function CreateScoreRow() {
-  return (
-    <tr>
-      <CreateEachScoreField order="1" />
-      <CreateEachScoreField order="2" />
-      <CreateEachScoreField order="3" />
-      <CreateEachScoreField order="4" />
-      <CreateTachiTortal tachi="first half" />
-      <CreateGrandTotal />
-      <CreateTachiTortal tachi="second half" />
-      <CreateEachScoreField order="8" />
-      <CreateEachScoreField order="7" />
-      <CreateEachScoreField order="6" />
-      <CreateEachScoreField order="5" />
-    </tr>
-  );
-}
+// function CreateScoreRow() {
+//   return (
+//     <tr>
+//       <CreateEachScoreField order="1" />
+//       <CreateEachScoreField order="2" />
+//       <CreateEachScoreField order="3" />
+//       <CreateEachScoreField order="4" />
+//       <CreateTachiTortal tachi="first half" />
+//       <CreateGrandTotal />
+//       <CreateTachiTortal tachi="second half" />
+//       <CreateEachScoreField order="8" />
+//       <CreateEachScoreField order="7" />
+//       <CreateEachScoreField order="6" />
+//       <CreateEachScoreField order="5" />
+//     </tr>
+//   );
+// }
 /**
  * 的中入力欄作成関数
  * @param {} props
  */
 function CreateEntryField(props) {
   //Todo clickで 〇 → ・（バツ）→ " "（空白）→ 〇 と入力可能にする
-  const [result, setResult] = useState("〇");
+  //const [result, setResult] = useState("〇");
+  const trial = props.trial;
+  // props.resultには一人の的中データ全部が入っている。。setResultでは更新分だけ置き換えてはくれない
+  // https://ja.reactjs.org/docs/hooks-faq.html#should-i-use-one-or-many-state-variables
   return (
     <td
       className={"player" + props.order}
       onClick={() => {
-        console.log({ result });
-        if (result == "〇") {
-          setResult("・");
-        } else if (result == "・") {
-          setResult(" ");
-        } else if (result == " ") {
-          setResult("〇");
+        console.log(props.result[trial]);
+        if (props.result[trial] == "〇") {
+          props.setResult({ ...props.result, [trial]: "・"});
+        } else if (props.result[trial] == "・") {
+          props.setResult({ ...props.result, [trial]: " "});
+        } else if (props.result[trial] == " ") {
+          props.setResult({ ...props.result, [trial]: "〇"});
         } else {
           alert("error");
         }
       }}
     >
-      {result}
+      {props.result[trial]}
     </td>
   );
 }
 CreateEntryField.propTypes = {
   order: PropTypes.string,
+  trial: PropTypes.string,
+  result: PropTypes.object,
+  setResult: PropTypes.func,
 };
 // 空欄
 function CreateEmpty() {
@@ -145,64 +152,104 @@ CreatePlayerNameField.propTypes = {
   name: PropTypes.string,
 };
 // 選手行作成関数
-function CreatePlayerRow() {
-  return (
-    <tr>
-      <CreatePlayerNameField name="A" />
-      <CreatePlayerNameField name="B" />
-      <CreatePlayerNameField name="C" />
-      <CreatePlayerNameField name="D" />
-      <CreateEmpty />
-      <CreatePlayerNameField name="あ大学" />
-      <CreateEmpty />
-      <CreatePlayerNameField name="H" />
-      <CreatePlayerNameField name="G" />
-      <CreatePlayerNameField name="F" />
-      <CreatePlayerNameField name="E" />
-    </tr>
-  );
-}
+// function CreatePlayerRow() {
+//   return (
+//     <tr>
+//       <CreatePlayerNameField name="A" />
+//       <CreatePlayerNameField name="B" />
+//       <CreatePlayerNameField name="C" />
+//       <CreatePlayerNameField name="D" />
+//       <CreateEmpty />
+//       <CreatePlayerNameField name="あ大学" />
+//       <CreateEmpty />
+//       <CreatePlayerNameField name="H" />
+//       <CreatePlayerNameField name="G" />
+//       <CreatePlayerNameField name="F" />
+//       <CreatePlayerNameField name="E" />
+//     </tr>
+//   );
+// }
 
-const TRIAL_COUNT = 20;
-let entryField = [];
-for (let i = 0; i < TRIAL_COUNT; i++) {
-  entryField.push(<CreateEntryField order="1" />);
-}
+// ReactDOM.render(
+//   <table width="98%" id="main-table">
+//     <tbody className="total">
+//       <CreateScoreRow />
+//     </tbody>
+//     <CreateTachi order="5" />
+//     <CreateTachi order="4" />
+//     <CreateTachi order="3" />
+//     <CreateTachi order="2" />
+//     <CreateTachi order="1" />
+//     <tbody className="player">
+//       <CreatePlayerRow />
+//     </tbody>
+//   </table>,
+//   document.getElementById("kyudo-table")
+// );
+
+/*********
+横ver
+*********/
+
 function CreatePlayer(props) {
-  console.log("player" + props.order);
+  // 将来的には射数もユーザーが入力
+  const TRIAL_COUNT = 20;
+  const initialAllResults = {};
+  for(let i = 0; i < TRIAL_COUNT; i++) {
+    initialAllResults["trial" + i] = "〇";
+  }
+  // 親で的中データを持っておく（状態を管理する？）
+  const [result, setResult] = useState(initialAllResults);
+  console.log(result);
+  //  playerの的中が変わるたび合計計算処理
+  // 結局親でやるしかない？このままだとCreateEachScoreFieldは多分いらない
+  let score = 0;
+  // 描画後でないとtableを取得できない
+  useEffect(() => {
+    const table = document.getElementById("test-table");
+    const cells = table.querySelectorAll("td.player" + props.order);
+    console.log(cells);
+    cells.forEach((cell) => {
+      if (cell.innerText == "〇") {
+        score++;
+      }
+      document.querySelector("th.player" + props.order).innerText = score;
+    });
+  });
+  
+  let entryField = [];
+  // keyが必要 https://ja.reactjs.org/docs/lists-and-keys.html
+  for (let i = 0; i < TRIAL_COUNT; i++) {
+    entryField.push(
+      <CreateEntryField
+        key={i}
+        order={props.order}
+        trial={"trial" + i}
+        result={result}
+        setResult={setResult}
+      />
+    );
+  }
   return (
     <tr>
-      <CreatePlayerNameField name="A" />
+      <CreatePlayerNameField name={props.name} />
       {entryField}
-      <CreateEachScoreField order={props.order} />
+      <CreateEachScoreField order={props.order} score={score} />
     </tr>
   );
 }
 CreatePlayer.propTypes = {
+  name: PropTypes.string,
   order: PropTypes.string,
 };
 
 ReactDOM.render(
-  <table width="98%" id="main-table">
-    <tbody className="total">
-      <CreateScoreRow />
-    </tbody>
-    <CreateTachi order="5" />
-    <CreateTachi order="4" />
-    <CreateTachi order="3" />
-    <CreateTachi order="2" />
-    <CreateTachi order="1" />
-    <tbody className="player">
-      <CreatePlayerRow />
-    </tbody>
-  </table>,
-  document.getElementById("kyudo-table")
-);
-
-ReactDOM.render(
-  <table width="98%">
+  <table width="98%" id="test-table">
     <tbody>
-      <CreatePlayer order="1" />
+      <CreatePlayer name="A" order="1" />
+      <CreatePlayer name="B" order="2" />
+      <CreatePlayer name="C" order="3" />
+      <CreatePlayer name="D" order="4" />
     </tbody>
   </table>,
   document.getElementById("test")
