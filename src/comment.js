3b7df24962b23d66b837af84fe9b464e6e432267
48ktk <ksao.257.urryh@gmail.com>
2020-10-25 00:46:54 +0900

    ルーティングの骨組みだけ

---
+++ src/comment.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './kyudo-table.css'
+
+ReactDOM.render(
+  <h1>comment</h1>,
+  document.getElementById('root')
+);
---
+++ src/toppage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './kyudo-table.css'
+
+ReactDOM.render(
+  <h1>Top</h1>,
+  document.getElementById('root')
+);
